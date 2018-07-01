7b6d1e6c91fd12f440a8dc3245f71a7385069d5e
waceic <caw5102@gmail.com>
2018-07-01 21:30:16 -0100

    testing service worker properly in github pages

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const cacheName = "currency-converter";
 
-// ----- 
+// -----
 self.addEventListener("install", event =>
   event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(["/"])))
 );
