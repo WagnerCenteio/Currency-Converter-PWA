28ae2d3fe7a09209ac6e39b864d9194425ad4743
waceic <caw5102@gmail.com>
2018-07-01 19:59:08 -0100

    sw changes

--- sw.js
+++ sw.js
@@ -1,15 +1,14 @@
 const cacheName = "currency-converter";
 
+// ----- 
 self.addEventListener("install", event =>
   event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(["/"])))
 );
 
+// ----- Hijacking fetch requests
 self.addEventListener("fetch", event => {
   const requestUrl = new URL(event.request.url);
 
-  console.log('requestUrl.origin',requestUrl.origin);
-  console.log('location.origin',location.origin);
-  
   if (requestUrl.origin === location.origin) {
     event.respondWith(caches.match("/"));
     return;
