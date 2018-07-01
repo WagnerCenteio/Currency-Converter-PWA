82faaf6cba5608c908315dff23c1962877afda13
waceic <caw5102@gmail.com>
2018-07-01 18:44:23 -0100

    adding index.html to the cache and serve it from there - ready for test

--- sw.js
+++ sw.js
@@ -5,5 +5,19 @@ self.addEventListener("install", event =>
 );
 
 self.addEventListener("fetch", event => {
-    
+  const requestUrl = new URL(event.request.url);
+
+  console.log('requestUrl.origin',requestUrl.origin);
+  console.log('location.origin',location.origin);
+  
+  if (requestUrl.origin === location.origin) {
+    event.respondWith(caches.match("/"));
+    return;
+  }
+
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
